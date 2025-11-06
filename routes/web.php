73df9a1d0e96308cddb0dc998d75dbf24e42f857
NamNT <namnt@ans-asia.com>
2025-11-06 10:09:11 +0700

    change

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |      
-*/                             
+*/                          
 
 // Route::get('/', function () {
 //     return view('welcome');       
 // });
-      
+
 // Route::get('/login', function () {
-//     return view('login');  
+//     return view('login');
 // });
-  
+
 Route::get('/layout', function () {
     return view('layout');
 });
